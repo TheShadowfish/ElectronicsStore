dce7c3b4d191ef7d2530f638d734182a88fbe3ec
TheShadowfish <shadowfish@yandex.ru>
2024-10-07 16:18:04 +0300

    product + contact - админка, наследование продукта, add swagger

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "suppliers",
     "django_filters",
     "rest_framework_simplejwt",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- suppliers/views.py
+++ suppliers/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -11,7 +13,29 @@ def redirect_to_admin(request):
     # Не набирать без конца адрес в браузере
     return redirect("admin/")
 
-
+@method_decorator(
+    name="list",
+    decorator=swagger_auto_schema(operation_description="Вывод списка поставщиков"),
+)
+@method_decorator(
+    name="create", decorator=swagger_auto_schema(operation_description="Создание нового поставщика")
+)
+@method_decorator(
+    name="destroy",
+    decorator=swagger_auto_schema(operation_description="Удаление выбранного поставщика"),
+)
+@method_decorator(
+    name="update",
+    decorator=swagger_auto_schema(operation_description="Обновление выбранного поставщика"),
+)
+@method_decorator(
+    name="partial_update",
+    decorator=swagger_auto_schema(operation_description="Обновление (частичное) выбранного поставщика"),
+)
+@method_decorator(
+    name="retrieve",
+    decorator=swagger_auto_schema(operation_description="Просмотр информации о поставщиках"),
+)
 class SupplierViewSet(ModelViewSet):
     queryset = Supplier.objects.all()
     permission_classes = (IsAuthenticated, IsActive)
